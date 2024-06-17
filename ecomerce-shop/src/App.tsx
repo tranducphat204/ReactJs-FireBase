80e741c262dc58fbf37fedff670c0f6932e90605
phatdeptraiii <vitphat350@gmail.com>
2024-06-17 11:08:23 +0700

    build source

---
+++ ecomerce-shop/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import Header from './Layouts/Client/Header';
+import Body from './Layouts/Client/Body';
+import Footer from './Layouts/Client/Footer';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ ecomerce-shop/src/Layouts/Client/Body.tsx
@@ -0,0 +1,9 @@
+function Body() {
+    return(
+        <div>
+            <h1>Hello BoDy</h1>
+        </div>
+    )
+}
+
+export default Body;
